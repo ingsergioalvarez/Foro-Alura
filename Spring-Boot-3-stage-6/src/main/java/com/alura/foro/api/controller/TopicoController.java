d971a818d4e901ce9110cba3f422edda6f1f2c40
ingsergioalvarez <156864617+ingsergioalvarez@users.noreply.github.com>
2024-07-07 21:36:28 -0600

    first charge
    Primer carga

---
+++ Spring-Boot-3-stage-6/src/main/java/com/alura/foro/api/controller/TopicoController.java
@@ -0,0 +1,81 @@
+package com.alura.foro.api.controller;
+
+import com.alura.foro.api.repository.TopicoRepository;
+import com.alura.foro.api.repository.UsuarioRepository;
+import com.alura.foro.api.dto.topico.*;
+import com.alura.foro.api.modelo.Curso;
+import com.alura.foro.api.modelo.Topico;
+import com.alura.foro.api.modelo.Usuario;
+import com.alura.foro.api.repository.CursoRepository;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/topicos")
+@SecurityRequirement(name = "bearer-key")
+public class TopicoController {
+
+    private final TopicoRepository topicoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final CursoRepository cursoRepository;
+
+    public TopicoController(TopicoRepository topicoRepository, UsuarioRepository usuarioRepository, CursoRepository cursoRepository) {
+        this.topicoRepository = topicoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.cursoRepository = cursoRepository;
+    }
+
+    @PostMapping
+    @SecurityRequirement(name = "bearer-key")
+    public ResponseEntity<DatosRespuestaTopico> registrar(@RequestBody @Valid DatosRegistroTopico datosRegistro, UriComponentsBuilder uri) {
+        Usuario autor = usuarioRepository.getReferenceById(datosRegistro.autorId());
+        Curso curso = cursoRepository.getReferenceById(datosRegistro.cursoId());
+        Topico topico = topicoRepository.save(new Topico(datosRegistro, autor, curso));
+        DatosRespuestaTopico datosRespuesta = new DatosRespuestaTopico(topico);
+        URI url = uri.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(url).body(datosRespuesta);
+    }
+
+    @GetMapping
+    @SecurityRequirement(name = "bearer-key")
+    public ResponseEntity<Page<DatosListadoTopico>> listar(@PageableDefault(size = 10)Pageable paginacion) {
+        return ResponseEntity.ok(topicoRepository.findAll(paginacion).map(DatosListadoTopico::new));
+    }
+
+    @GetMapping("/{id}")
+    @SecurityRequirement(name = "bearer-key")
+    public ResponseEntity<DatosRespuestaTopicoId> retornaDatos(@PathVariable Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosRespuestaTopicoId(topico));
+    }
+
+    @PutMapping
+    @SecurityRequirement(name = "bearer-key")
+    @Transactional
+    public ResponseEntity<DatosRespuestaTopico> actualizar(@RequestBody @Valid DatosActualizarTopico datosActualizar) {
+        Usuario autor = usuarioRepository.getReferenceById(datosActualizar.autorId());
+        Curso curso = cursoRepository.getReferenceById(datosActualizar.cursoId());
+        Topico topico = topicoRepository.getReferenceById(datosActualizar.id());
+        topico.actualizarDatos(datosActualizar, autor, curso);
+        return ResponseEntity.ok( new DatosRespuestaTopico(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @SecurityRequirement(name = "bearer-key")
+    @Transactional
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        //topicoRepository.delete(topico);
+        topico.cerrarTopico();
+        return ResponseEntity.noContent().build();
+    }
+}
